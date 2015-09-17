8161c74fd54a823193ddf82ebe6260a432d96d25
caleb tebbe <calebtebbe@gmail.com>
2015-09-17 09:16:58 -0600

    fixing parsed chunking logic

--- src/cs555/tebbe/node/ChunkNode.java
+++ src/cs555/tebbe/node/ChunkNode.java
@@ -5,15 +5,20 @@ import cs555.tebbe.util.*;
 import java.io.*;
 import java.net.*;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ChunkNode implements Node {
+
+    public static final int MAJOR_HB_SECONDS = 60*3;
+    public static final int MINOR_HB_SECONDS = MAJOR_HB_SECONDS/10;
 
     public static final int DEFAULT_SERVER_PORT = 18080;
     public static final String BASE_DIR = "./";
 
     private NodeConnection _Controller = null;
-    private TCPServerThread serverThread = null; // listens for incoming client nodes
+    private TCPServerThread serverThread = null;                                // listens for incoming client nodes
     private HashMap<String, ChunkStorage> storedChunksMap = new HashMap<>();
+    private List<ChunkStorage> newChunksStored = new ArrayList<>();             // holds data items for minor heartbeats
 
     public ChunkNode(String host, int port) {
         try {
@@ -31,7 +36,7 @@ public class ChunkNode implements Node {
             System.out.println("IOException thrown contacting ControllerNode:"+ioe.getMessage());
             System.exit(0);
         }
-        new Timer().schedule(new MinorHeartbeat(), 0, 1000);
+        new Timer().schedule(new HeartbeatTaskManager(), 0, MINOR_HB_SECONDS*1000);
     }
 
     public synchronized void onEvent(Event event){
@@ -49,19 +54,12 @@ public class ChunkNode implements Node {
         System.out.println("** Store chunk event received");
         System.out.println("File name: " + event.getFileName());
         System.out.println("Byte size: " + event.getBytesToStore().length);
-        System.out.println("Bytes: " );
-        try {
-            System.out.write(event.getBytesToStore());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println();
 
         ChunkStorage record = new ChunkStorage(event.getFileName(), "0.1", event.getChunkSequenceID(), new Date().getTime());
         BufferedOutputStream writer = null;
         try {
             writer = new BufferedOutputStream(new FileOutputStream(
-                    new File(record.getChunkStorageName())));
+                    new File(BASE_DIR+record.getChunkStorageName())));
             saveChunk(writer, event);
         } catch (IOException e) {
             e.printStackTrace();
@@ -71,6 +69,7 @@ public class ChunkNode implements Node {
             }
         }
 
+        System.out.println(event.getNextHost());
         storedChunksMap.put(record.getFileName(), record);
     }
 
@@ -91,10 +90,23 @@ public class ChunkNode implements Node {
             System.out.println("Usage: java ChunkNode controller_host controller_port");
         }
     }
-     private class MinorHeartbeat extends TimerTask {
-         @Override
-         public void run() {
-             System.out.println("task");
-         }
-     }
+
+    /*
+        sends heartbeats to the _Controller
+        every ratioMinorToMajor heartbeats, a major heartbeat is sent
+        otherwise a minor heartbeat is sent
+     */
+    private class HeartbeatTaskManager extends TimerTask {
+
+        private final int ratioMinorToMajor = 10;
+        private AtomicInteger numHeartbeats = new AtomicInteger(0);
+
+        @Override public void run() {
+            if(numHeartbeats.getAndIncrement() % ratioMinorToMajor == 0) {
+                System.out.println("major heartbeat");
+            } else
+                System.out.println("minor heartbeat");
+            //_Controller.sendEvent(null);
+        }
+    }
 }
